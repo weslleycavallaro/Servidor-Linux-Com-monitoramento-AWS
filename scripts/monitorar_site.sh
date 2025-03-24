6f52e485d8de25fca1903d5c6fd180673813e048
Weslley Cavallaro <90354207+weslleycavallaro@users.noreply.github.com>
2025-03-24 11:45:43 -0300

    Create monitorar_site.sh

---
+++ scripts/monitorar_site.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+source variaveis.env
+
+# Local do arquivo onde os logs serao gravados
+ARQUIVO_LOG="/var/log/monitoramento.log"
+
+# Resposta HTTP recebida da URL
+RESPOSTA_HTTP=$(curl -o /dev/null -s -w "%{http_code}" $URL)
+
+# Armazena a data atual para salvar na log do sistema
+DATA_ATUAL=$(date "+%Y-%m-%d %H:%M:%S")
+
+# Compara se a resposta HTTP recebida possui o codigo 200(OK)
+if [ "$RESPOSTA_HTTP" -eq 200 ]; then
+	# Grava a resposta como ok no arquivo de log
+  	echo "$DATA_ATUAL - O SITE ESTA OPERANDO COMO O ESPERADO. Codigo: $RESPOSTA_HTTP" >> $ARQUIVO_LOG
+else
+	# Grava a resposta como falha no arquivo de log
+  	echo "$DATA_ATUAL - FALHA AO ACESSAR O SITE. Codigo: $RESPOSTA_HTTP" >> $ARQUIVO_LOG
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"A aplicacao apresentou falha! Verifique o arquivo de log."}' $WEBHOOK 
+fi
